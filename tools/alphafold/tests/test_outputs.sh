896218c6f92440e71ba14367e2ad5dfff95b4d1f
Cameron Hyde <chyde@neoformit.com>
2023-02-14 13:50:55 +1000

    Update test_outputs.sh script

--- tools/alphafold/tests/test_outputs.sh
+++ tools/alphafold/tests/test_outputs.sh
@@ -1,12 +1,46 @@
 #!/usr/bin/env bash
 
+# You will probably need to create a venv and 'pip install -r alphafold/requirements.txt'
+# for the script to be able to read the pickle outputs as they contain jax objects.
+
 set -e
 
-echo "TEST multimer with per-residue scores"
-python gen_extra_outputs.py test-data/multimer_output -p -m
+EXPECT_OUTPUTS="
+test-data/monomer_output/plddts.tsv
+test-data/monomer_output/model_confidence_scores.tsv
+test-data/monomer_output/model_1.pkl
+test-data/monomer_output/model_2.pkl
+test-data/monomer_output/model_3.pkl
+test-data/monomer_output/model_4.pkl
+test-data/monomer_output/model_5.pkl
+test-data/multimer_output/plddts.tsv
+test-data/multimer_output/model_confidence_scores.tsv
+test-data/multimer_output/model_1.pkl
+test-data/multimer_output/model_2.pkl
+test-data/multimer_output/model_3.pkl
+test-data/multimer_output/model_4.pkl
+test-data/multimer_output/model_5.pkl"
 
+echo "TEST monomer output with per-residue scores"
+python outputs.py test-data/monomer_output -p --model-pkl
+
+echo ""
+echo "TEST multimer output with per-residue scores"
+python outputs.py test-data/multimer_output -p -m --model-pkl
+
+for path in $EXPECT_OUTPUTS; do
+  if [ ! -f $path ]; then
+    echo "FAIL: output file '$path' not found"
+    exit 1
+  fi
+done
+
+echo ""
 echo "Removing output data..."
-rm test-data/multimer_output/plddts.tsv \
-   test-data/multimer_output/model_confidence_scores.tsv
+rm -f \
+   test-data/*mer_output/plddts.tsv \
+   test-data/*mer_output/model_confidence_scores.tsv \
+   test-data/*mer_output/model_*.pkl
 
+echo ""
 echo "PASS"
