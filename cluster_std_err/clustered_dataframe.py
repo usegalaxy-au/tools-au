991cb898dab967301d1003c8b6ab529d9416e428
Tom Harrop <twharrop@gmail.com>
2023-03-22 16:41:54 +1100

    get rid of word movers

--- cluster_std_err/clustered_dataframe.py
+++ cluster_std_err/clustered_dataframe.py
@@ -6,11 +6,9 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.cluster import DBSCAN
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from transformers import AutoTokenizer, AutoModel
 import Levenshtein
 import numpy as np
 import pandas as pd
-import torch
 
 
 class ClusteredDataFrame(pd.DataFrame):
@@ -28,7 +26,6 @@ class ClusteredDataFrame(pd.DataFrame):
         self.tokenize_err()
 
         # Run the similarity calculations
-        # self.compute_wmd_similarity(model_name='codebert')
         self.compute_levenshtein_similarity()
         self.compute_jaccard_similarity()
         self.compute_tfidf_similarity()
@@ -58,71 +55,6 @@ class ClusteredDataFrame(pd.DataFrame):
 
             preprocessed_errors.append(' '.join(filtered_tokens))
         self['tokenized_err'] = preprocessed_errors
-
-    def compute_wmd_similarity(self, model_name: str):
-        """
-        Computes the Word Mover's Distance (WMD) similarity between error
-        messages using the specified pre-trained model.
-
-        Parameters:
-            error_messages (List[str]): List of error messages to cluster.
-            model_name (str): Name of the pre-trained model to use.
-            Options: 'codebert', 'codegpt'.
-
-        Returns:
-            np.ndarray: Similarity matrix, where the (i, j) element is the WMD
-            similarity between error_messages[i] and error_messages[j].
-        """
-        if model_name not in ('codebert', 'codegpt'):
-            raise ValueError("Invalid pre-trained model name. Options: 'codebert', 'codegpt'.")
-
-        # Load pre-trained model and tokenizer
-        if model_name == 'codebert':
-            model = AutoModel.from_pretrained('microsoft/codebert-base')
-        else:  # model_name == 'codegpt'
-            model = AutoModel.from_pretrained('microsoft/codegpt-small-java')
-
-        tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-        # Pre-process error messages and compute word embeddings
-        embeddings = []
-        tokenized_errors = []
-        error_messages = self.error_messages
-
-        for error in error_messages:
-            # Tokenize and convert to input IDs
-            tokens = tokenizer(
-                error,
-                return_tensors='pt',
-                padding=True,
-                truncation=True)
-            input_ids = tokens['input_ids']
-
-            # Get word embeddings from pre-trained model
-            with torch.no_grad():
-                output = model(input_ids)
-                if model_name == 'codebert':
-                    embeddings.append(output.last_hidden_state[:, 0, :].numpy())
-                else:  # model_name == 'codegpt'
-                    embeddings.append(output[0][:, -1, :].numpy())
-
-            # Save tokenized error for later use
-            tokenized_errors.append(tokenizer.tokenize(error))
-
-        # Compute similarity matrix using Word Mover's Distance
-        sim_matrix = np.zeros((len(embeddings), len(embeddings)))
-        for i in range(len(embeddings)):
-            for j in range(i, len(embeddings)):
-                if i == j:
-                    sim_matrix[i, j] = 1.0
-                else:
-                    distance = model.wmdistance(
-                        tokenized_errors[i],
-                        tokenized_errors[j])
-                    sim_matrix[i, j] = sim_matrix[j, i] = 1 / (1 + distance)
-            
-        # Store the similarity matrix in the object
-        self.wmd_similarity_matrix = sim_matrix
 
     def compute_levenshtein_similarity(self):
         preprocessed_errors = self.tokenized_err
@@ -164,7 +96,6 @@ class ClusteredDataFrame(pd.DataFrame):
             eps=eps,
             min_samples=min_samples)
         similarity_matrices = {
-            # 'wmd': self.wmd_similarity_matrix,
             'levenshtein': self.levenshtein_similarity_matrix,
             'jaccard': self.jaccard_similarity_matrix,
             'tfidf': self.tfidf_similarity_matrix,
