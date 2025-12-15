d0b898f26d535eec99ad07572a9952072bb19794
Cameron Hyde <chyde@neoformit.com>
2025-12-15 10:36:44 +1000

    Modify phastest docker image to mount dbs

--- tools/phastest/dev/run.sh
+++ tools/phastest/dev/run.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+INPUT_FILE="test.fna"
+IMAGE="phastest"
+INPUT_TYPE="seq"
+
+while getopts "loco" opt; do
+    case $opt in
+        l) INPUT_FILE="test_long.fna" ;;
+        o) IMAGE="phastest-original" ;;
+        c) INPUT_TYPE="contig" ;;
+        *) echo "Invalid option"; exit 1 ;;
+    esac
+done
+
+if [ "$INPUT_TYPE" == "seq" ]; then
+    INPUT_FILE="seq_$INPUT_FILE"
+    INPUT_MODE="fasta"
+else
+    INPUT_FILE="contig_$INPUT_FILE"
+    INPUT_MODE="contig"
+fi
+
+echo "Using image:      $IMAGE"
+echo "Input mode:       $INPUT_MODE"
+echo "Input file: $INPUT_FILE"
+
+
 docker run \
     -v ./test-data:/phastest_inputs \
     -v ./jobs/:/root/phastest-app/JOBS \
-    -it \
-    phastest -i contig -s contig_test_long.fna
+    -v ./refdata/DB:/root/phastest-app/DB \
+    $IMAGE -i $INPUT_MODE -s $INPUT_FILE
