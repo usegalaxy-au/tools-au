198aa8dafc13b5c6c1c701d67e54ba3d180c4afd
Cameron Hyde <chyde@neoformit.com>
2025-12-18 12:28:45 +1000

    Tidy up phastest tool

--- tools/phastest/dev/run.sh
+++ tools/phastest/dev/run.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
-INPUT_FILE="test.fna"
 IMAGE="phastest"
+INPUT_FILE="test.fna"
 INPUT_TYPE="seq"
+DEEP_MODE="lite"
+DATA_DIR="./test-data"
 
-while getopts "loco" opt; do
+while getopts "lcd" opt; do
     case $opt in
         l) INPUT_FILE="test_long.fna" ;;
-        o) IMAGE="phastest-original" ;;
         c) INPUT_TYPE="contig" ;;
+        d) DEEP_MODE="deep" ;;
         *) echo "Invalid option"; exit 1 ;;
     esac
 done
 
-if [ "$INPUT_TYPE" == "seq" ]; then
-    INPUT_FILE="seq_$INPUT_FILE"
-    INPUT_MODE="fasta"
+shift $((OPTIND - 1))
+
+if [[ $1 != "" ]]; then
+    INPUT_FILE=$(basename "$1")
+    DATA_DIR=$(realpath "$(dirname "$1")")
+    if [[ "$INPUT_TYPE" == "seq" ]]; then
+        INPUT_MODE="fasta"
+    else
+        INPUT_MODE="contig"
+    fi
 else
-    INPUT_FILE="contig_$INPUT_FILE"
-    INPUT_MODE="contig"
+    if [[ "$INPUT_TYPE" == "seq" ]]; then
+        INPUT_FILE="seq_$INPUT_FILE"
+        INPUT_MODE="fasta"
+    else
+        INPUT_FILE="contig_$INPUT_FILE"
+        INPUT_MODE="contig"
+    fi
 fi
 
-echo "Using image:      $IMAGE"
-echo "Input mode:       $INPUT_MODE"
-echo "Input file: $INPUT_FILE"
+echo "Using image: $IMAGE"
+echo "Input mode:  $INPUT_MODE"
+echo "Input file:  $INPUT_FILE"
+echo "Data dir:    $DATA_DIR"
 
 
 docker run \
-    -v ./test-data:/phastest_inputs \
+    -v "$DATA_DIR:/phastest_inputs" \
     -v ./jobs/:/root/phastest-app/JOBS \
     -v ./refdata/DB:/root/phastest-app/DB \
-    $IMAGE phastest -i $INPUT_MODE -s $INPUT_FILE
+    $IMAGE -i $INPUT_MODE -s $INPUT_FILE -m $DEEP_MODE --yes
