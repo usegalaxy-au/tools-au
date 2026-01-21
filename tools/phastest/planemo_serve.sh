2a74b4fe38fdee78e8ee41f1ce95ea701de64043
Cameron Hyde <chyde@neoformit.com>
2026-01-21 17:04:49 +1000

    Prepare phastest for toolshed

--- tools/phastest/planemo_serve.sh
+++
@@ -1,5 +0,0 @@
-planemo serve \
-    --no_cleanup \
-    --docker \
-    --docker_run_extra_arguments \
-    "-v /home/cameron/dev/galaxy/tool-dev/tools-au/tools/phastest/dev/refdata/DB:/home/phastest/phastest-app/DB:ro"
---
+++ tools/phastest/wrapper/planemo_serve.sh
@@ -0,0 +1,8 @@
+planemo serve \
+    --no_cleanup \
+    --docker \
+    --docker_run_extra_arguments \
+    "-v /data/phastest:/data/phastest:ro -e PHASTEST_DB_PATH=/data/phastest"
+
+    # To test with DB dir mounted directly:
+    # "-v /data/phastest:/home/phastest/phastest-app/DB:ro"
